3852b763c4102c5e046adb43eafb5a73090e8485
prabhjeevnijjar <prabhjeevnijjar@gmail.com>
2024-06-04 14:32:46 +0530

    new nav

--- components/layout/TopNav.jsx
+++ components/layout/TopNav.jsx
@@ -6,7 +6,7 @@ const TopNav = () => {
   const pathname = usePathname();
 
   return (
-    <div className="top-0 w-[100%] h-14 shadow-lg rounded">
+    <div className="top-0 w-[100%] h-14 shadow-lg  bg-white border border-gray-200 rounded-lg">
       <h1 className="text-center pt-4 text-[20px] font-medium">{pathname === '/my-library' ? 'My Library' : 'Activity Log'}</h1>
     </div>
   );
